5705eaa1d4eb015d2c81bc4907fee9ef7a466a0d
TheRaidoX <raidolehtla@live.com>
2024-01-22 12:22:16 +0200

    adding @media

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
+const openBtn = document.querySelector('.icon-open');
+const closeBtn = document.querySelector('.icon-close');
 
 navToggle.addEventListener('click', () => {
-  primaryNav.hasAttribute('data-visible')
-    ? navToggle.setAttribute('aria-expanded', false)
-    : navToggle.setAttribute('aria-expanded', true);
-  primaryNav.toggleAttribute('data-visible');
+  const isVisible = primaryNav.hasAttribute('data-visible');
+
+  if (isVisible) {
+    navToggle.setAttribute('aria-expanded', false);
+    primaryNav.removeAttribute('data-visible');
+    openBtn.style.display = 'block';
+    closeBtn.style.display = 'none';
+  } else {
+    navToggle.setAttribute('aria-expanded', true);
+    primaryNav.setAttribute('data-visible', '');
+    openBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+  }
 });
