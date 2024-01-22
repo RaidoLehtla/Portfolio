5c4f31a7f6a6f0058f0ca11572394ca617b1bccd
TheRaidoX <raidolehtla@live.com>
2024-01-22 21:41:50 +0200

    changed illustation and fixed nav buttons

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const primaryNav = document.querySelector('.primary-navigation');
-const openBtn = document.querySelector('.icon-open');
-const closeBtn = document.querySelector('.icon-close');
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.mobile-nav-toggle');
+  const primaryNav = document.querySelector('.primary-navigation');
+  const openBtn = document.querySelector('.icon-open');
+  const closeBtn = document.querySelector('.icon-close');
+  const navLinks = document.querySelectorAll('.nav-list a');
 
-navToggle.addEventListener('click', () => {
-  const isVisible = primaryNav.hasAttribute('data-visible');
+  navToggle.addEventListener('click', toggleNav);
 
-  if (isVisible) {
-    navToggle.setAttribute('aria-expanded', false);
-    primaryNav.removeAttribute('data-visible');
-    openBtn.style.display = 'block';
-    closeBtn.style.display = 'none';
-  } else {
-    navToggle.setAttribute('aria-expanded', true);
-    primaryNav.setAttribute('data-visible', '');
-    openBtn.style.display = 'none';
-    closeBtn.style.display = 'block';
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      if (primaryNav.hasAttribute('data-visible')) {
+        toggleNav();
+      }
+    });
+  });
+
+  function toggleNav() {
+    const isVisible = primaryNav.hasAttribute('data-visible');
+
+    if (isVisible) {
+      navToggle.setAttribute('aria-expanded', false);
+      primaryNav.removeAttribute('data-visible');
+      openBtn.style.display = 'block';
+      closeBtn.style.display = 'none';
+    } else {
+      navToggle.setAttribute('aria-expanded', true);
+      primaryNav.setAttribute('data-visible', '');
+      openBtn.style.display = 'none';
+      closeBtn.style.display = 'block';
+    }
   }
 });
